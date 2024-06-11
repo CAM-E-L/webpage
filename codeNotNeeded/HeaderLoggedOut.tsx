e8064f0cc3290b2a6af6fe01649633964955ffad
Camel-app <camel.app@proton.me>
2024-06-11 09:07:51 +0200

    headers remove isSupabaseConnected

--- codeNotNeeded/HeaderLoggedOut.tsx
+++ codeNotNeeded/HeaderLoggedOut.tsx
@@ -1,3 +1,5 @@
+import { createClient } from "@/utils/supabase/server";
+
 import ButtonHome from "./Buttons/Home";
 import ButtonInformation from "./Buttons/Information";
 import ButtonGetStarted from "./Buttons/GetStarted";
@@ -7,7 +9,17 @@ import ButtonRegister from "./Buttons/Register";
 import ButtonLogin from "./Buttons/Login";
 
 
-
+const canInitSupabaseClient = () => {
+  // This function is just for the interactive tutorial.
+  // Feel free to remove it once you have Supabase connected.
+  try {
+    createClient();
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+const isSupabaseConnected = canInitSupabaseClient();
 
 export default function HeaderLoggedOut() {
 
@@ -20,8 +32,8 @@ export default function HeaderLoggedOut() {
       <ButtonOnlineDocumentation/>
       </div>
       <div className=".w-2/5 flex space-x-10 items-center p-5 text-sm justify-end">
-      <ButtonRegister/>
-      <ButtonLogin/>
+      {isSupabaseConnected && <ButtonRegister/>}
+      {isSupabaseConnected && <ButtonLogin/>}
     </div>
   </nav>
   );
