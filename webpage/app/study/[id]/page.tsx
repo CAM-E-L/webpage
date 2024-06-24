e0187e761bb9fd88506a5965d76980c04047e702
Camel-app <camel.app@proton.me>
2024-06-24 11:56:00 +0200

    update UI single study

--- webpage/app/study/[id]/page.tsx
+++ webpage/app/study/[id]/page.tsx
@@ -23,18 +23,33 @@ export default async function SingleStudy({
     return redirect("/error");
   } else {
     // get data from the database for respective study and researcher
-    const { data: singleStudy } = await supabase
-      .from("studies")
+    const { data: collectedCAMs } = await supabase
+      .from("collectedcams")
       .select()
-      .eq("namestudy", params.id)
-      .eq("email", data.user.email);
+      .eq("namestudy", params.id);
 
-    // redirect to error page if no data is found
-    if (singleStudy?.length === 0 || singleStudy === undefined) {
-      return redirect("/error");
-    }
+    var arrayNumConcepts: number[] = [];
+    var arrayAvgValence: number[] = [];
+    var arrayDateEnd: Date[] = [];
+    collectedCAMs?.forEach((element) => {
+      arrayNumConcepts.push(element.numconcepts);
+      arrayAvgValence.push(element.avgvalence);
+      arrayDateEnd.push(element.dateend);
+    });
 
-    console.log("singleStudy", singleStudy);
+    const average = (arr: any[]) => arr.reduce((p, c) => p + c, 0) / arr.length;
+
+    if(arrayDateEnd.length != 0){
+    // Convert the date strings to Date objects and find the maximum date
+    var latestDate = new Date(
+      Math.max(...arrayDateEnd.map((date) => new Date(date).getTime()))
+    );
+
+    // Convert the latest Date object back to a string if needed
+    var latestDateString = latestDate.toISOString();
+  }else{  
+    var latestDateString = "No data available";
+  }
 
     return (
       <main className="w-full max-w-6xl text-xl animate-in">
@@ -43,33 +58,39 @@ export default async function SingleStudy({
 
         <div className="flex justify-center space-x-5 flex-row mb-5">
           <div className="flex-col flex w-64">
-            <p className="font-bold">{params.id}</p> participants have joined
-            your study
+            <p className="font-bold">{collectedCAMs?.length}</p> participants
+            have joined your study
           </div>
           <div className="flex-col flex w-64">
-            <p className="font-bold">{params.id}</p> concepts on average have
-            been drawn
+            <p className="font-bold">
+              {arrayNumConcepts.length > 0 ? Math.round(average(arrayNumConcepts) * 100) / 100 : "No data available"}
+            </p>{" "}
+            concepts on average have been drawn
           </div>
           <div className="flex-col flex w-64">
-            <p className="font-bold">{params.id}</p> is the mean valence of the
-            drawn concepts
+            <p className="font-bold">
+            {arrayAvgValence.length > 0 ? Math.round(average(arrayAvgValence) * 100) / 100 : "No data available"}
+            </p>{" "}
+            is the mean valence of the drawn concepts
           </div>
           <div className="flex-col flex w-64">
-            <p className="font-bold">{params.id}</p> was the last time you
-            collected a CAM
+            <p className="font-bold">{latestDateString}</p> was the last time
+            you collected a CAM
           </div>
         </div>
 
-        <table className="table-auto text-left text-xl mx-auto w-full">
+        <table className="table-auto text-left text-lg mx-auto w-full">
           <thead>
-            <tr className="text-xl">
+            <tr className="text-lg">
               <th className="border border-gray-300 px-2 py-2">CAM ID</th>
               <th className="border border-gray-300 px-2 py-2">
                 Participant ID
               </th>
               <th className="border border-gray-300 px-2 py-2">Date Start</th>
               <th className="border border-gray-300 px-2 py-2">Date End</th>
-              <th className="border border-gray-300 px-2 py-2">Duration (in min.)</th>
+              <th className="border border-gray-300 px-2 py-2">
+                Duration (in min.)
+              </th>
               <th className="border border-gray-300 px-2 py-2">
                 Number of drawn concepts
               </th>
@@ -83,38 +104,47 @@ export default async function SingleStudy({
             </tr>
           </thead>
           <tbody>
-            {singleStudy?.map((study, index) => (
+            {collectedCAMs?.map((study, index) => (
               <tr
-                key={study.namestudy}
+                key={study.camid}
                 className={`border-y-4 border-black ${
                   index % 2 === 0 ? "bg-gray-200" : "bg-white"
                 }`}
               >
                 <td className="py-5 border border-gray-300 px-5">
-                  {study.namestudy}
+                  {study.camid}
                 </td>
                 <td className="py-5 border border-gray-300 px-5">
-                  {new Date(study.creation_date).toLocaleDateString() +
-                    " (" +
-                    new Date(study.creation_date).toLocaleTimeString() +
-                    ")"}
+                  {study.participantid}
                 </td>
-                <td className="py-5 border border-gray-300 px-5">
-                  {"Placeholder" + index}
+                <td className="py-5 border border-gray-300 px-5 text-sm">
+                  {study.datestart}
                 </td>
-                <td className="py-5 border border-gray-300 px-5">
-                  {"Placeholder" + index}
+                <td className="py-5 border border-gray-300 px-5 text-sm">
+                  {study.dateend}
                 </td>
-                <td className="py-5 border border-gray-300 px-5">aa</td>
-                <td className="py-5 border border-gray-300 px-5">aa</td>
-                <td className="py-5 border border-gray-300">bb</td>
-                <td className="py-5 border border-gray-300">cc</td>
+                <td className="py-5 border border-gray-300 px-5 text-center">
+                  {" "}
+                  {study.datediff}
+                </td>
+                <td className="py-5 border border-gray-300 px-5 text-center">
+                  {" "}
+                  {study.numconcepts}
+                </td>
+                <td className="py-5 border border-gray-300 text-center">
+                  {" "}
+                  {study.numconnectors}
+                </td>
+                <td className="py-5 border border-gray-300 text-center">
+                  {" "}
+                  {study.avgvalence}
+                </td>
                 <td className="py-5 border border-gray-300">
                   <Link
                     href={""}
                     className="px-1 text-blue-500 hover:underline"
                   >
-                    to draw CAM
+                    draw CAM
                   </Link>
                 </td>
               </tr>
